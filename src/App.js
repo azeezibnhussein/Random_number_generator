d50f255d5305021c886a9c602ee9b9e555743d86
Abdulazeez Olafimihan <olafimihanabdulazeez@gmail.com>
2022-12-08 01:57:00 +0100

    random_number_generator/1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(20);
+  const [output, setOutput] = useState(
+    Math.floor(Math.random() * (max - min + 1) + min)
+  );
+  const randomOutput = () => {
+    setOutput(Math.floor(Math.random() * (max - min + 1) + min));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="showcase">
+        <div className="instructions-section">
+          <p className="output">
+            Random Number: <span>{output}</span>
+          </p>
+          <p>Click the button below to generate random number</p>
+        </div>
+
+        <div className="random-number-container">
+          <div className="num">
+            <p>Min:</p>
+            <input
+              type="number"
+              value={min}
+              onChange={(e) => setMin(+e.target.value)}
+            />
+          </div>
+
+          <div className="num">
+            <p>Max:</p>
+            <input
+              type="number"
+              value={max}
+              onChange={(e) => setMax(+e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="btn">
+          <button onClick={randomOutput}>Click here</button>
+        </div>
+      </div>
     </div>
   );
 }
